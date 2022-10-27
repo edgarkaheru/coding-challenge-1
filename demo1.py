32f1a93532d2225ea034e72eb249ac24741aa4b8
egdar040 <bwambaleedgar28@gmail.comgit config --global user.email bwambaleedgar28@gmail.com>
2022-10-27 16:43:31 +0300

    first submission

---
+++ demo1.py
@@ -0,0 +1,13 @@
+class Person():
+    def __init__(self,name, email, phone_number):
+        self.name = name 
+        self.email = email
+        self. hone_number = phone_number
+        
+        
+class Student(Person):
+    def __int__(self,students_id,course):
+        self.students_id = students_id
+        self.course = course
+        
+        
